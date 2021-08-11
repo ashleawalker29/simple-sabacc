21592a4809b5779e92ff327aedca2756065c80a1
Ashlea Walker <ashleawalker29@gmail.com>
2021-08-11 17:17:44 -0600

    Added logic for cards in the negative value range.
    Changed the card print method as to not override print().

--- cards/card.py
+++ cards/card.py
@@ -4,7 +4,7 @@ class Card(object):
 
     def __init__(self, value, stave):
         if ((type(value) is not int) or
-                (value > 10 or value <= 0) or
+                (value > 10 or value == 0 or value < -10) or
                 (stave not in self.STAVES) or
                 (stave == 'Sylop') or
                 (value is None or stave is None)):
@@ -15,7 +15,7 @@ class Card(object):
             self.value = value
             self.stave = stave
 
-    def print(self):
+    def print_card(self):
         if self.stave == 'Sylop':
             print(self.stave)
         else:
