5b3b48f7593d5027a1b198913be5fae2ca72bae1
Ashlea Walker <ashleawalker29@gmail.com>
2021-08-11 17:15:48 -0600

    Changed the name of the card print method so as to not override print().

--- cards/test_card_print.py
+++ cards/test_card_print.py
@@ -9,27 +9,27 @@ class CardTests(unittest.TestCase):
     @patch('builtins.print')
     def test_card_print_sylop(self, mock_print):
         test_card = Card(0, 'Sylop')
-        test_card.print()
+        test_card.print_card()
 
         mock_print.assert_called_with('Sylop')
 
     @patch('builtins.print')
     def test_card_print_circles(self, mock_print):
         test_card = Card(5, 'Circles')
-        test_card.print()
+        test_card.print_card()
 
         mock_print.assert_called_with('5 of Circles')
 
     @patch('builtins.print')
     def test_card_print_triangles(self, mock_print):
         test_card = Card(5, 'Triangles')
-        test_card.print()
+        test_card.print_card()
 
         mock_print.assert_called_with('5 of Triangles')
 
     @patch('builtins.print')
     def test_card_print_squares(self, mock_print):
         test_card = Card(5, 'Squares')
-        test_card.print()
+        test_card.print_card()
 
         mock_print.assert_called_with('5 of Squares')
