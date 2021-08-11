0e510c224c30cc4e2b8cf671e89025b3c5ef750b
Ashlea Walker <ashleawalker29@gmail.com>
2021-08-11 09:14:21 -0600

    Added additional defaulting cards to test and applicable logic.

--- cards/card.py
+++ cards/card.py
@@ -7,6 +7,7 @@ class Card(object):
             (type(value) is not int) or
             (value > 10 or value <= 0) or
             (stave not in self.STAVES) or
+            (stave == 'Sylop') or
             (value is None or stave is None)):
 
             self.value = 0
--- cards/test_card.py
+++ cards/test_card.py
@@ -2,25 +2,30 @@ import unittest
 
 from card import Card
 
+
 class CardTests(unittest.TestCase):
 
     def test_defaulted_card(self):
-        test_cards = [Card(None, None), Card('test', None), Card(11, 'Triangles'), Card(0, 'Triangles')]
+        test_cards = [Card(None, None), Card('test', None), Card(None, 'Circles'),
+                      Card(5, 'Test Stave'), Card(0, 'Sylop'), Card(5, 'Sylop'),
+                      Card(0, 'Triangles')]
 
         for test_card in test_cards:
             self.assertEqual(test_card.value, 0)
             self.assertEqual(test_card.stave, 'Sylop')
 
     def test_value_is_int(self):
-        test_cards = [Card(None, None), Card('test', None), Card(5, 'Circles'),
-                      Card(11, 'Triangles'), Card(0, 'Triangles')]
+        test_cards = [Card(None, None), Card('test', None), Card(None, 'Circles'),
+                      Card(5, 'Test Stave'), Card(0, 'Sylop'), Card(5, 'Sylop'),
+                      Card(0, 'Triangles'), Card(5, 'Circles')]
 
         for test_card in test_cards:
             self.assertTrue(type(test_card.value) is int)
 
     def test_value_between_0_and_10(self):
-        test_cards = [Card(None, None), Card('test', None), Card(5, 'Circles'),
-                      Card(11, 'Triangles'), Card(0, 'Triangles')]
+        test_cards = [Card(None, None), Card('test', None), Card(None, 'Circles'),
+                      Card(5, 'Test Stave'), Card(0, 'Sylop'), Card(5, 'Sylop'),
+                      Card(0, 'Triangles'), Card(5, 'Circles')]
 
         for test_card in test_cards:
             self.assertTrue(0 <= test_card.value <= 10)
@@ -28,7 +33,8 @@ class CardTests(unittest.TestCase):
     def test_is_staves_valid(self):
         # Test cards that should default to Card(0, 'Sylop')
         test_cards = [Card(None, None), Card('test', None), Card(None, 'Circles'),
-                      Card(5, 'Test Stave'), Card(0, 'Sylop'), Card(5, 'Sylop'), Card(0, 'Triangles')]
+                      Card(5, 'Test Stave'), Card(0, 'Sylop'), Card(5, 'Sylop'),
+                      Card(0, 'Triangles')]
 
         for test_card in test_cards:
             self.assertTrue(test_card.stave, 'Sylop')
